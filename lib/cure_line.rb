b646d3b9b49bb93dedb850b0e7601a032f839481
sue445 <sue445@sue445.net>
2018-03-21 00:58:56 +0900

    Impl CureLine.user

--- lib/cure_line.rb
+++ lib/cure_line.rb
@@ -7,4 +7,15 @@ module CureLine
   autoload :ResourceMethods, "cure_line/resource_methods"
   autoload :TimeMethods,     "cure_line/time_methods"
   autoload :User,            "cure_line/user"
+
+  # @param user_id [String]
+  #
+  # @return [CureLine::User]
+  #
+  # @example
+  #   # read https://timeline.line.me/user/_dYbbV3vmaJrvqBoV5ZlpCbPN2CWUZdDQayBvjBE
+  #   user = CureLine.user("_dYbbV3vmaJrvqBoV5ZlpCbPN2CWUZdDQayBvjBE")
+  def self.user(user_id)
+    User.new(user_id)
+  end
 end
